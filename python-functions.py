b43a66e25ecd3a091906a890d6ad004faaadc15a
Minji <dudvnd13@gmail.com>
2022-08-01 21:06:36 -0700

    finish exercise

--- python-functions.py
+++ python-functions.py
@@ -10,3 +10,29 @@ print(sum_to(6))
 
 # number 2
     
+def largest(nums):
+  largest = 0
+  for n in nums:
+    if n > largest:
+      largest = n
+  return largest
+
+print(largest([1, 2, 3, 4, 0]))
+
+# number 3
+
+def occurrences(s1, s2):
+    return s1.count(s2)
+
+print(occurrences('fleep floop', 'e'))
+
+# number 4
+
+def product(*args):
+    product = 1
+    for arg in args:
+        product *= arg
+    return product
+
+print(product(-1, 4))
+        
